703da9ef1ac15c4a14fb71e34a5b52f73883be12
aniqatc <aniqatc97@gmail.com>
2024-12-28 03:41:09 -0500

    class => className

--- src/components/LoadingCard.js
+++ src/components/LoadingCard.js
@@ -5,7 +5,7 @@ import Card from "./Card";
 function LoadingCard() {
     return (
         <Card className="animate-up delay-1">
-            <i class="bi bi-hourglass-split"></i> Loading content...
+            <i className="bi bi-hourglass-split"></i> Loading content...
         </Card>
     )
 }
